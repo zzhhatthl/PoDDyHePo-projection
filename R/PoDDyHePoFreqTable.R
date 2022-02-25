22f7c356d6d46c6554c73e6169cb158fb10741aa
zzhhatthl <zhi.zhou@thl.fi>
2022-02-26 00:18:07 +0200

    Func updates.

--- R/PoDDyHePoFreqTable.R
+++ R/PoDDyHePoFreqTable.R
@@ -3,7 +3,7 @@
 #'
 #' @description It returns a table arranged by Gender and Year.
 #'
-#' @details The table includes Total, Missing amount and its proportion (%).
+#' @details The table includes total, respondents (N) and its proportion (%).
 #'
 #' @param data A data frame. It could the well-formatted data, or the data frame whose prevalences are to be estimated.
 #' @param colName A column that you want to creat a frequency table for.
@@ -21,20 +21,20 @@ PoDDyHePoFreqTable <- function(data, colName){
   options(qwraps2_markup = "markdown")
   summary_statistic <- list("Male" =
                               list(
-                                "Total" = ~ sum(!is.na(get("year")[which(sex == 1)])),
-                                "Missing" = ~ sum(is.na(get(colName)[which(sex == 1)])),
-                                "%" = ~ round(100 * mean(is.na(get(colName)[which(sex == 1)])), 1)
+                                "Total" = ~ sum(!is.na(get("vuosi")[which(sukup == 1)])),
+                                "Missing" = ~ sum(!is.na(get(colName)[which(sukup == 1)])),
+                                "%" = ~ round(100 * mean(!is.na(get(colName)[which(sukup == 1)])), 1)
                               ),
                             "Female" =
                               list(
-                                "Total" = ~ sum(!is.na(get("year")[which(sex == 2)])),
-                                "Missing" = ~ sum(is.na(get(colName)[which(sex == 2)])),
-                                "%" = ~ round(100 * mean(is.na(get(colName)[which(sex == 2)])), 1)
+                                "Total" = ~ sum(!is.na(get("vuosi")[which(sukup == 2)])),
+                                "Missing" = ~ sum(!is.na(get(colName)[which(sukup == 2)])),
+                                "%" = ~ round(100 * mean(!is.na(get(colName)[which(sukup == 2)])), 1)
                               )
   )
 
   print(qwraps2::summary_table(
-    group_by(data, get("year")),
+    group_by(data, get("vuosi")),
     summary_statistic
   ))
 
